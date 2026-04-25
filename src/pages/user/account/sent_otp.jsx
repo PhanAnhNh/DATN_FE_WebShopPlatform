48deabdb1f65496ed32e41357e67ba1c33349c1a
PhanAnhNh <anhnhat1322004@gmail.com>
2026-04-25 19:25:39 +0700

    jj

--- src/pages/user/account/sent_otp.jsx
+++ src/pages/user/account/sent_otp.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Toast from '../../../components/common/Toast';
 import { BACKEND_URL } from '../../../config'
-import { userApi } from '../../api/api';
+import { userApi } from '../../../api/api';
 
 function VerifyOTP() {
   const [otpCode, setOtpCode] = useState(["", "", "", "", "", ""]);
